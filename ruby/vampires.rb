285e5f361855a71256a4528d753539b188dda3c8
fernandoamadorii@gmail.com <fernandoamadorii@Fernandos-MacBook-Air.local>
2016-08-03 19:29:56 -0700

    finishes release 4

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,8 @@
+puts "How many employees will be processed?"
+employees_processed = gets.to_i
+
+until employees_processed == 0 
+
 puts "What is your name?"
 name = gets.chomp
 
@@ -5,7 +10,7 @@ puts "How old are you?"
 age = gets.to_i
 
 puts "What year were you born?"
-year_born = gets.chomp
+year_born = gets.to_i
 
 puts "Our company cafeteria serves garlic bread. Should we order some for you?"
 order_garlic_bread = gets.chomp
@@ -13,18 +18,43 @@ order_garlic_bread = gets.chomp
 puts "Would you like to enroll in the company's health insurance?"
 health_insurance = gets.chomp
 
+puts "Do you have any allergies?"
+allergies = gets.chomp
+
+while allergies != "done"
+	if allergies == "sunshine"
+		puts "Probably a vampire."
+	else 
+		puts "Do you have any allergies?"
+	end
+	allergies = gets.chomp
+end
+
 age_verification = 2016 - age
 
-if (year_born == age_verification) && (order_garlic_bread == "yes" || health_insurance == "yes")
-	puts "Probably not a vampire."
-elsif (year_born != age_verification) && (order_garlic_bread == "no" || health_insurance == "no")
-	puts "Probably a vampire."
-elsif (year_born != age_verification) && (order_garlic_bread == "no") && (health_insurance == "no")
-	puts "Almost certainly a vampire."
-elsif name == "Drake Cula" || name == "Tu Fang" 
+if name == "Drake Cula" || name == "Tu Fang"
 	puts "Definitely a vampire." 
 else 
-	puts "Results inconclusive."
+	if year_born == age_verification
+		if order_garlic_bread == "yes" || health_insurance == "yes"
+			puts "Probably not a vampire."
+		else
+			puts "Probably a vampire."
+		end
+	elsif year_born != age_verification
+		if order_garlic_bread == "no" && health_insurance == "no"
+			puts "Almost certainly a vampire."
+		elsif order_garlic_bread == "no" || health_insurance == "no"
+			puts "Probably a vampire."
+		else 
+			puts "Probably not a vampire."
+		end
+	else 
+		puts "Results inconclusive."
+	end
 end
-	
+
+employees_processed = employees_processed - 1
+
+end
 	
