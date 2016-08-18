92bc77c09f91fef9e1022a30f81f870550317460
fernandoamadorii@gmail.com <fernandoamadorii@Fernandos-MacBook-Air.local>
2016-08-18 06:18:30 -0700

    created gps2_2

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,18 +1,62 @@
-
-Santa.class
+class Santa
 	
-	def initialize 
+	def initialize(gender, ethnicity) 
 		p "Initializing Santa instance ..."
+		@gender = gender
+		@ethnicity = ethnicity
+		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		
 	end 
 	
 	def speak
-		p "Ho, ho, ho! Haaaappy holidays!"
+		p "#{@gender} Santa says: Ho, ho, ho! Haaaappy holidays!"
 	end 
 	
 	def eat_milk_and_cookies(cookie)
 		p "That was a good *{<type of cookie here}!"
 	end
 	
+	def celebrate_birthday(age)
+		@age = age + 1 
+	end
+	
+	def get_mad_at(reindeer_name)
+		reindeer_ranking.delete(reindeer_name)
+		reindeer_ranking << reindeer_name
+	end
+	
+	def gender=(new_gender)
+		@gender = new_gender
+	end
+	
+	def age
+		@age
+	end
+	
+	def ethnicity
+		@ethnicity
+	end
+		
 end 
 
-claus = Santa.new
+
+=begin
+santas = []
+genders = ["agender", "female", "bigender", "male", "female", 
+	"gender fluid", "N/A"]
+ethnicities = ["black", "Latino", "white", "Japanese-African", 
+	"prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+	
+genders.length.times {|i| santas << Santa.new(genders[i], ethnicities[i])}
+
+p santas
+
+santas.each do |santa|
+	puts "-------"
+	santa.speak
+end 
+=end
+
+
+claus = Santa.new 
+claus.celebrate_birthday(55)
