76a5b9086d91a4e9132e512a97693f97ff3b28a3
fernandoamadorii@gmail.com <fernandoamadorii@Fernandos-MacBook-Air.local>
2016-08-18 12:31:12 -0700

    finishes 6.3

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,12 +1,12 @@
 class Santa
-	attr_reader :age, :ethnicity
-	attr_accessor :gender
+	attr_reader :ethnicity
+	attr_accessor :gender, :age
 	def initialize(santa_gender, santa_ethnicity) 
 		p "Initializing Santa instance ..."
-		gender = santa_gender
+		@gender = santa_gender
 		@ethnicity = santa_ethnicity
 		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		age = 0 
+		@age = 0 
 	end 
 	def speak
 		p "#{@gender} Santa says: Ho, ho, ho! Haaaappy holidays!"
@@ -15,7 +15,7 @@ class Santa
 		p "That was a good *{<type of cookie here}!"
 	end
 	def celebrate_birthday(age)
-		@age = age + 1 
+		@age = age + 1
 	end
 	def get_mad_at(reindeer_name)
 		@reindeer_ranking.delete(reindeer_name)
@@ -23,8 +23,10 @@ class Santa
 	end
 end 
 
+# DRIVER CODE 
 
-=begin
+# This makes an array with the genders and ethnicities, then takes the
+# genders and runs the speak method with the genders. 
 santas = []
 genders = ["agender", "female", "bigender", "male", "female", 
 	"gender fluid", "N/A"]
@@ -32,26 +34,24 @@ ethnicities = ["black", "Latino", "white", "Japanese-African",
 	"prefer not to say", "Mystical Creature (unicorn)", "N/A"]
 	
 genders.length.times {|i| santas << Santa.new(genders[i], ethnicities[i])}
-
 p santas
-
 santas.each do |santa|
 	puts "-------"
 	santa.speak
 end 
-=end
 
-
+#checks that the getter and setter methods work
 claus = Santa.new("male", "Latino") 
 claus.celebrate_birthday(55)
 claus.get_mad_at("Vixen")
 puts "This Santa is #{claus.age} years old and #{claus.ethnicity}."
 
 
-
-
-
-
-
-
+# Build Many, Many Santas with genders and ethnicities and changes Santa's age
+5.times do 
+	claus = Santa.new(genders.sample, ethnicities.sample) 
+	puts "#{claus.gender} and #{claus.ethnicity}"
+	claus.age = rand(1...140)
+	puts "They are #{claus.age} years old"
+end
 
