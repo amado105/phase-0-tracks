7a5d54894034f7b916d559ba623988b7c0b88f31
fernandoamadorii@gmail.com <fernandoamadorii@Fernandos-MacBook-Air.local>
2016-09-01 16:56:23 -0700

    Creates an array and hash with data, and iterates using eeach and map

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -9,3 +9,37 @@ end
 house_words { |words1, words2| puts "#{words1} with #{words2}!"}
 
 
+
+# use of .each and .map!
+
+house_sigil = {
+	stark: "wolf", 
+	arryn: "falcon",
+	lannister: "lion",
+	tully: "fish"
+}
+
+westerosi_cities = [
+	"Winterfell remains", 
+	"Riverrun has fallen", 
+	"Castle Black remains", 
+	"King's Landing has fallen"
+]
+
+house_sigil.each do |house, sigil|
+	puts "House #{house.capitalize} waves its flags bearing a virtuous #{sigil}!"
+end 
+
+westerosi_cities.each do |location|
+	if location.include?("remains")
+		puts "#{location} and still stands with us!"
+	else 
+		puts "#{location} and belongs to the enemy."
+	end 
+end 
+
+westerosi_cities.map! do |location|
+	location + "!"
+end 
+
+
