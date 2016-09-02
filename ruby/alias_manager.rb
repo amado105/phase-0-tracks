5d24a69676059dc6345ff26dd543fac01f86ce6d
fernandoamadorii@gmail.com <fernandoamadorii@Fernandos-MacBook-Air.local>
2016-09-02 10:38:41 -0700

    creates a user interface and hash to collect all the names and then present the data structure of all names and aliases

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -33,3 +33,24 @@ def encrypt(str)
 	end 
     p new_name
 end
+
+# Creates the empty hash
+hash = {}
+
+# User interface
+puts "What is your name? (type 'quit' to exit)"
+name = gets.chomp
+
+# This will run as long as the user types in anything but quit
+until name == "quit"
+	name2 = name.split(" ").reverse!
+	name3 = name2.join(" ")
+	name4 = name3.split("")
+	new_name = encrypt(name4)
+	hash[name] = new_name
+	puts "What is your name? (type 'quit' to exit)"
+	name = gets.chomp
+end
+
+# presents the data structure of all the names
+hash.each {|real_name, fake_name| puts "#{real_name} is now named #{fake_name}."}
