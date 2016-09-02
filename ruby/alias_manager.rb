a9621a9b86134e3dd95c0f026aa49eead352d72a
fernandoamadorii@gmail.com <fernandoamadorii@Fernandos-MacBook-Air.local>
2016-09-02 10:37:36 -0700

    adds method that changes the letter to the new letter

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -11,3 +11,25 @@
 # if letter includes u make it a a
 
 
+# This method takes the name and changes the letters
+def encrypt(str)
+	letters = "aeioubcdfghjklmnpqrstvwxyzAEIOBCDFGHJKLMNPQRSTVWXYZ"
+	new_name = ""
+	str.length.times do |index|
+		if str[index] == "u"
+			new_name << "a"
+		elsif str[index] == "U"
+			new_name << "A"
+		elsif str[index] == "z"
+			new_name << "b"
+		elsif str[index] == "Z"
+			new_name << "B"
+		elsif str[index] == " "
+			new_name << " "
+		else
+			next_letter = letters[letters.index(str[index]) + 1]
+			new_name << next_letter
+		end
+	end 
+    p new_name
+end
