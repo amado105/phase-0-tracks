0c2a0564371bcbcc7e20c62546b141026e88e722
fernandoamadorii@gmail.com <fernandoamadorii@Fernandos-MacBook-Air.local>
2016-09-04 22:18:42 -0700

    Makes pseudocode on how to write a method that takes array of integers and an integer, searching for the specific integer in array

---
+++ ruby/problem_solving_methods.rb
@@ -0,0 +1,11 @@
+
+# iterate through arr, moving every item into a new array. 
+# Break if the last item of an array 
+# is == to the integer searching for. 
+# if the last item of the new array is == integer, 
+# then take the length of the new array and subtract by 1, 
+# making this a new variable called index and print 
+# else p nil 
+
+
+
