82bbce2d573dc5ae06f88f488bac96162e0f7a7b
fernandoamadorii@gmail.com <fernandoamadorii@Fernandos-MacBook-Air.local>
2016-09-08 10:00:21 -0700

    initializes some attributes and tests out the code with new array

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -24,3 +24,41 @@ end
 # santa = Santa.new 
 # santa.speak
 # santa.eat_milk_and_cookies("chocolate-chip")
+
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+
+# The code above is the same as the code below
+
+# santas = []
+# gender = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# ethnicity = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# gender.length.times do |i|
+#   santas << Santa.new(gender[i], ethnicity[i])
+# end
+
+# The code above is the same as the code below
+
+gender = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+ethnicity = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+santas = []
+gender.length.times {|i| santas << Santa.new(gender[i], ethnicity[i])}
+p santas
+santas.each do |santa|
+	puts "-------"
+	santa.speak
+end 
+
+
+
+
+
+
+
+
