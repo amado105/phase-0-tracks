acdf643d7e5de240549bb401a932fb2074266c5d
fernandoamadorii@gmail.com <fernandoamadorii@Fernandos-MacBook-Air.local>
2016-09-12 15:14:07 -0700

    Adds small rspec file

--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -1,6 +1,13 @@
 require_relative 'game'
 
 describe Game do 
-  let(:game) {Game.new}
+  let(:game) {Game.new("paella")}
+
+    it "change word into underscores string" do 
+      array = ["p", "a", "e", "l", "l", "a"]
+    expect(game.empty_array(array)).to eq "_ _ _ _ _ _"
+  end 
+
+# Sorry, no time to do more. 
 
 end 
